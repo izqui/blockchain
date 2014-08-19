409413adcbba6500bcacee5e3481e40339c513d1
Jorge Izquierdo <jorge@izqui.me>
2014-08-18 23:14:12 -0400

    Transaction and block

--- main.go
+++ main.go
@@ -2,36 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/izqui/helpers"
-	"reflect"
-	"time"
 )
-
-const (
-	TRANSACTION_COMPLEXITY = 2
-)
-
-func (t *Transaction) Init() []byte {
-
-	prefix := helpers.ArrayOfBytes(TRANSACTION_COMPLEXITY, 0) // [0 0]
-	for !reflect.DeepEqual(t.Hash()[0:TRANSACTION_COMPLEXITY], prefix) {
-
-		t.Tries += 1
-	}
-
-	return t.Hash()
-}
-func (t *Transaction) Hash() []byte {
-
-	txt := fmt.Sprintf("%s%s%s%s%s", t.From, t.To, t.Data, t.Timestamp, t.Tries)
-	return helpers.SHA256([]byte(txt))
-}
 
 func main() {
 
-	t := Transaction{From: "Jorge", To: "Jonathan", Data: "Hello", Timestamp: time.Now().Unix()}
-	fmt.Println(t)
-	fmt.Printf("%x\n", t.Init())
-	fmt.Println(t.Hash())
-	fmt.Println(t.Tries)
+	keypair := GenerateNewKeypair(POW_PREFIX, 1)
+	fmt.Println(string(keypair.Public))
 }
--- transaction.go
+++ transaction.go
@@ -1,10 +1,52 @@
 package main
 
+import (
+	"time"
+)
+
 type TransactionSlice []Transaction
+
 type Transaction struct {
-	From      string
-	To        string
-	Data      string
-	Timestamp int64
-	Tries     int // nonce
+	Header    TransactionHeader
+	Payload   []byte
+	Signature []byte
+}
+
+type TransactionHeader struct {
+	From          []byte
+	To            []byte
+	Timestamp     int
+	ContentLength int
+	ContentHash   []byte
+}
+
+// Returns bytes to be sent to the network
+
+func (t Transaction) Sign(keypair Keypair) Transaction {
+
+	newT := t
+	newT.Header.Timestamp = int(time.Now().Unix())
+
+	headerBytes, _ := t.Header.MarshalBinary()
+	newT.Signature, _ = keypair.Sign(headerBytes)
+	return newT
+}
+
+func (t *Transaction) MarshalBinary() ([]byte, error) {
+
+	headerBytes, _ := t.Header.MarshalBinary()
+	return (append(append(headerBytes, t.Payload...), t.Signature...)), nil
+}
+
+func (t *Transaction) UnmarshalBinary() []byte {
+	return []byte{}
+}
+
+func (th TransactionHeader) MarshalBinary() ([]byte, error) {
+
+	return []byte{}, nil
+}
+
+func (th TransactionHeader) UnmarshalBinary() []byte {
+	return []byte{}
 }
